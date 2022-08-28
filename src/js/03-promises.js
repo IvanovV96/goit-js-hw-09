133f8cd654d5b559d7233155eae0de2a85fbe604
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-08-28 21:37:05 +0300

    add creator promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from "notiflix"
+
+const form = document.querySelector('.form')
+const firstDelay = form.elements.delay
+const step = form.elements.step
+const amount = form.elements.amount
+let pos = 0
+
+
+
+  
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+    let delay = parseInt(firstDelay.value)
+   const intervald = setInterval(() => {
+    const stepInt = parseInt(step.value)
+    pos += 1
+    delay += stepInt
+    if(pos === +amount.value) {
+      clearInterval(intervald)
+    }
+    createPromise(pos, delay)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+    })
+   }, delay)
+  
   }
-}
+)
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.5;
+  return new Promise((resolve, reject) => {
+     setTimeout(()=> {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+     }, firstDelay.value)
+    })
+  }
+
+
+
